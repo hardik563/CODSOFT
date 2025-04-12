d4e7c86b8dacb0c34aebc5ed4e6c6e560e832b99
hardik563 <hardikdhamija676@gmail.com>
2025-04-12 15:47:33 +0530

    COMMIT

--- src/StudentGradecalculatortask2.java
+++ src/StudentGradecalculatortask2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class StudentGradecalculatortask2 {
-    public static void main(String args[]) {
+    public static void main(String []args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of subjects= ");
         int totalsubjects= sc.nextInt();
